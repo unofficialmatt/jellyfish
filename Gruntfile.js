a52b7ab15be2cda90909feb81c4f28f8ff1df080
Matt Weet <hello@mattweet.com>
2019-03-23 10:45:39 +0000

    Concat/Uglift .js files and more

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function (grunt) {
     // Project configuration
     grunt.initConfig({
         
+        // Remove old images
+        clean: ['assets/img/*'], 
+        
         // Define which sass files should be compiled
         sass: {
             options: {
@@ -11,7 +14,7 @@ module.exports = function (grunt) {
             dist: {
                 files: [
                     {
-                        src: 'src/scss/base.scss',
+                        src: 'build/scss/base.scss',
                         dest: 'assets/css/style.min.css'
                     }
                 ]
@@ -34,18 +37,41 @@ module.exports = function (grunt) {
             }
         },
         
-        // Compress images in src/img/, output to assets/img
+        // Compress images in build/img/, output to assets/img
         imagemin: {
             dynamic: {
                 files: [{
                     expand: true,
-                    cwd: 'src/img/',
+                    cwd: 'build/img/',
                     src: ['**/*.{png,jpg,JPG,JPEG,jpeg,svg,gif}'],
                     dest: 'assets/img/'
                 }]
             }
+        },    
+
+        concat: {
+          options: {
+                sourceMap: false,
+                separator: ';\n'
+          },
+
+          site: {
+            src: ['build/js/vendor/*.js', 'build/js/site/*.js'],
+            dest: 'assets/js/site.js',
+          },
         },
 
+        uglify: {
+          options: {
+            mangle: false
+          },
+          site: {
+            files: {
+              'assets/js/site.min.js': ['assets/js/site.js']
+            }
+          }
+        },           
+        
         watch: {
             
             // rerun $ grunt when the Gruntfile is edited
@@ -63,17 +89,22 @@ module.exports = function (grunt) {
                     reload: true,
                     event: ['changed', 'added', 'deleted']
                 },
-                files: ['assets/scss/**/*.scss'],
+                files: ['build/scss/**/*.scss'],
                 tasks: [ 'sass', 'postcss']
             },
+
+            concat_js: {
+                files: ['build/js/**/*.js'],
+                tasks: ['concat', 'uglify']
+            },            
             
             images: {
                 options: {
                     reload: true,
                     event: ['changed', 'added', 'deleted']
                 },
-                files: ['src/img/**/*.{png,jpg,JPG,JPEG,jpeg,svg,gif}'],
-                tasks: [ 'imagemin' ]
+                files: ['build/img/**/*.{png,jpg,JPG,JPEG,jpeg,svg,gif}'],
+                tasks: [ 'clean', 'imagemin' ]
             }
 
         }
@@ -86,14 +117,20 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
 
     // Copy required scripts from node_modules to assets
     grunt.registerTask('copyScripts', function () {
-        var jquery = grunt.file.read('node_modules/jquery/dist/jquery.min.js');
-        grunt.file.write('assets/js/jquery.min.js', jquery);
+        var jquery = grunt.file.read('node_modules/jquery/dist/jquery.js');
+        
+        grunt.file.write('build/js/vendor/jquery.js', jquery);
     });
     
     // Default Grunt task, runs via $ grunt
-    grunt.registerTask('default', ['copyScripts', 'sass', 'postcss', 'imagemin', 'watch']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['copyScripts', 'concat', 'uglify', 'sass', 'postcss', 'clean', 'imagemin', 'watch']);
 
 };
