330b01c954f0c8e868f2066086355f5d8b0b5186
Matt Weet <matt@epls.design>
2021-11-01 11:10:08 +0000

    Fixes for GA tracking

--- assets/js/ga-events.js
+++ assets/js/ga-events.js
@@ -1,36 +1,42 @@
 /**
  * Send interaction events to Google Analytics
  */
-(function ($) {
-  if (typeof gtag == "function") {
-    // Send mailto: events
-    $('a[href^="mailto:"]').click(function () {
+ (function ($) {
+
+  // Send mailto: events
+  $('a[href^="mailto:"]').click(function () {
+    if (typeof gtag !== 'undefined') {
       gtag("event", "contact", {
         event_category: "Email Enquiry",
         event_action: "Mailto Click",
         event_label: $(this).attr("href"),
       });
       return true;
+      }
     });
 
     // Send tel: events
     $('a[href^="tel:"]').click(function () {
-      gtag("event", "contact", {
-        event_category: "Telephone Enquiry",
-        event_action: "Tel Link Click",
-        event_label: $(this).attr("href"),
-      });
-      return true;
+      if (typeof gtag !== 'undefined') {
+        gtag("event", "contact", {
+          event_category: "Telephone Enquiry",
+          event_action: "Tel Link Click",
+          event_label: $(this).attr("href"),
+        });
+        return true;
+      }
     });
 
     // Send *.pdf download events
     $('a[href$=".pdf"]').click(function () {
-      gtag("event", "contact", {
-        event_category: "PDF Download",
-        event_action: "Download",
-        event_label: $(this).attr("href"),
-      });
-      return true;
+      if (typeof gtag !== 'undefined') {
+        gtag("event", "contact", {
+          event_category: "PDF Download",
+          event_action: "Download",
+          event_label: $(this).attr("href"),
+        });
+        return true;
+      }
     });
 
     // Send external link clicks
@@ -39,18 +45,20 @@
         document.domain +
         "'],[href*='mailto'],[href*='tel'],a[href$='.pdf'])"
     ).click(function (event) {
-      // Prevent # and javascript links being sent
-      if (
-        $(this).attr("href").charAt(0) != "#" &&
-        !$(this).attr("href").startsWith("javascript")
-      ) {
-        gtag("event", "contact", {
-          event_category: "External Link",
-          event_action: "Link Click",
-          event_label: $(this).attr("href"),
-        });
-        return true;
+      if (typeof gtag !== 'undefined') {
+        // Prevent # and javascript links being sent
+        if (
+          $(this).attr("href").charAt(0) != "#" &&
+          !$(this).attr("href").startsWith("javascript")
+        ) {
+          gtag("event", "contact", {
+            event_category: "External Link",
+            event_action: "Link Click",
+            event_label: $(this).attr("href"),
+          });
+          return true;
+        }
       }
     });
-  }
+
 })(jQuery);
