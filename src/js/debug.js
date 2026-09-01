9801a0968f65f6faa15373e622d00771e842d0ab
Matt Weet <matt@weareboba.com>
2026-09-01 20:16:39 +0100

    Updates all colour tokens with new -fg versions. this will be replaced again later by a more modern approach. removes jf debug

--- src/js/debug.js
+++
@@ -1,5 +0,0 @@
-function jfDebug() {
-  // Trigger debug mode by applying .jf-debug to document
-  var docBody = document.getElementsByTagName("body")[0];
-  docBody.classList.toggle("jf-debug");
-}
